fb3fe0b349d263c96a009553a4a0aa7b6ef92f5a
Bob yu <y9y9j8@163.com>
2022-01-04 15:55:46 -0600

    增加两个构造函数，三个公开属性，方便用户调用 add two constructor and three property

--- Course/2-1DataStructure/Array1.cs
+++ Course/2-1DataStructure/Array1.cs
@@ -8,5 +8,37 @@ namespace _2_1DataStructure
 {
     class Array1
     {
+        private int[] data;
+        private int N;
+
+        public Array1(int capacity)//当用户确定数组长度时，使用此构造函数
+        {
+            data = new int[capacity];
+            N = 0;
+        }
+
+        public Array1() : this(10) { }//此构造函数与下面的作用一样，是一种简单写法
+
+        //让用户访问数组信息,三个公开的属性
+        public int capacity//得到长度
+        {
+            get { return data.Length; }
+        }
+
+        public int count//实际记录元素的数量
+        {
+            get { return N; }
+        }
+
+        public bool IsEmpty//判断动态数组是否为空
+        {
+            get { return N == 0; }
+        }
+        //public Array1()//当用户不确定数组长度时，使用此构造函数（多态polymorphism）
+        //{
+        //    data = new int[10];//避免空间浪费，开一个默认空间（10）
+        //    N = 0;
+        //}
+
     }
 }
